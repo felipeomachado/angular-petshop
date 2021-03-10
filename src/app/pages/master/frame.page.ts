02a8f7fa94ed3acbfb8c48304e0b5e9ebee7031c
Felipe Alexandre <felipeomachado@hotmail.com>
2021-03-09 22:09:20 -0300

    master page created

---
+++ src/app/pages/master/frame.page.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+
+@Component({
+  selector: 'app-frame-page',
+  template: '<app-navbar></app-navbar><router-outlet></roter-outlet>'
+})
+export class FramePageComponent {
+
+}
