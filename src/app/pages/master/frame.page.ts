80d690fa66c851413cad032d0ad5d52b678251e0
Felipe Alexandre <felipeomachado@hotmail.com>
2021-03-09 22:14:40 -0300

    routes configured

--- src/app/pages/master/frame.page.ts
+++ src/app/pages/master/frame.page.ts
@@ -3,7 +3,7 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: 'app-frame-page',
-  template: '<app-navbar></app-navbar><router-outlet></roter-outlet>'
+  template: '<app-navbar></app-navbar><router-outlet></router-outlet>'
 })
 export class FramePageComponent {
 
